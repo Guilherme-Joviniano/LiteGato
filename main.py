85eecb4b223949904999996d3ad9c6c44dcd0586
Guilherme Joviniano de Sousa <73798396+Guilherme-Joviniano@users.noreply.github.com>
2021-11-05 12:02:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
     dp.add_handler(CommandHandler('help', help_command))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
-    updater.start_polling(10)
+    updater.start_polling(2)
     updater.idle()
 
 main()
